1b387207df05b6c20c36ecf865e2975614ab24cc
dmorais.eng.comp <dmorais.eng.comp@gmail.com>
2020-01-10 21:36:59 -0300

    API with NodeJS pratics.

---
+++ backend_NodeJS/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors')
+
+//Iniciando App
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Connect db
+mongoose.connect(
+  'mongodb+srv://morais_dio:dfm000000@node0-wk3go.mongodb.net/mercado?retryWrites=true&w=majority',{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+});
+
+requireDir('./src/models');
+
+app.use('/api',require('./src/routes'))
+
+app.listen(3001);
